10ea698744221817f1b9eb90d2c8f4cbb8a74856
zyhe <zhuoyuan@pku.edu.cn>
2025-03-27 13:56:15 +0800

    test 1

---
+++ run.sh
@@ -0,0 +1,35 @@
+# ###
+ # @Author: Zhuoyuan He
+ # @Date: 2025-03-27 13:53:27
+ # @LastEditors: Zhuoyuan He
+ # @LastEditTime: 2025-03-27 13:53:34
+ # @FileName: run.sh
+ # @Description: 
+ # 
+### 
+
+#!/bin/bash
+
+# check for root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# print system info
+uname -a
+cat /etc/os-release
+
+# install dependencies
+apt-get update
+
+# install neofetch
+apt-get install neofetch -y
+neofetch
+
+apt-install build-essential -y
+apt-install cmake -y
+apt-install git -y
+
+
+
